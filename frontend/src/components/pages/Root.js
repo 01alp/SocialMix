6915db224004c6779a8a0f5f125db33b0330c69e
01alp <alp01code@gmail.com>
2023-12-06 15:32:30 +0200

    follow

--- frontend/src/components/pages/Root.js
+++ frontend/src/components/pages/Root.js
@@ -3,15 +3,24 @@ import { Outlet } from 'react-router-dom';
 import { UsersContextProvider } from '../store/users-context';
 import { WebSocketContextProvider } from '../store/websocket-context';
 import Layout from '../layouts/Layout';
+import { FollowingContextProvider } from '../store/following-context';
+import { GroupsContextProvider } from '../store/groups-context';
+import { JoinedGroupContextProvider } from '../store/joined-group-context';
 
 const Root = () => {
   return (
     <>
       <UsersContextProvider>
         <WebSocketContextProvider>
-          <Layout>
-            <Outlet />
-          </Layout>
+          <FollowingContextProvider>
+            <GroupsContextProvider>
+              <JoinedGroupContextProvider>
+                <Layout>
+                  <Outlet />
+                </Layout>
+              </JoinedGroupContextProvider>
+            </GroupsContextProvider>
+          </FollowingContextProvider>
         </WebSocketContextProvider>
       </UsersContextProvider>
     </>
---
+++ frontend/src/components/store/groups-context.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+export const GroupsContext = React.createContext({
+  groups: [],
+  onNewGroupCreated: () => {},
+});
+
+export const GroupsContextProvider = (props) => {
+  const [groupsList, setGroupsList] = useState([]);
+
+  // get groups
+  const getGroupsHandler = () => {
+    const groupUrl = 'http://localhost:8080/group';
+    fetch(groupUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log('group (context): ', data);
+        let [groupsArr] = Object.values(data);
+        setGroupsList(groupsArr);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(getGroupsHandler, []);
+
+  return (
+    <GroupsContext.Provider
+      value={{
+        groups: groupsList,
+        onNewGroupCreated: getGroupsHandler,
+      }}
+    >
+      {props.children}
+    </GroupsContext.Provider>
+  );
+};
